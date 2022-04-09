3dd7821d77632edf748ded703c8f082e1c491526
samuelferreiras <contatosamueiferrreira@gmail.com>
2022-04-09 18:17:03 -0300

    correcting error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 
 import "../stylesheets/application"
 
-require('./exchanges')
+require("./exchanges")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
